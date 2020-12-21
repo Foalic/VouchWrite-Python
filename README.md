5918307169641dc56577b285a96befa3b90894df
Foalic <fionn.utsch@posteo.net>
2020-12-21 16:27:09 +0000

    Added a short readme describing general functionality.
    Adding overview of main steps and features that need programming in 
    vwrite.py.

---
+++ README.md
@@ -0,0 +1,4 @@
+# VoucherWrite
+
+This is an application which enables the user to upload a desired background image/pdf and insert an input into a chosen position.
+Essentially, it is an application to fill out reusable templates (of forms, etc.) that are not in text format. 
--- vwrite.py
+++ vwrite.py
@@ -1,2 +1,14 @@
 from tkinter import *
 from reportlab.pdfgen.canvas import Canvas
+
+# 1) Create a window
+
+# 2) Upload a pdf (if image= would be great) and scale window o it
+
+# 3) Allow input boxes to be pulled into place
+
+# 4) Print input onto the pdf (canvas)
+
+# 5) [Merge pdf with written input or] create a new pdf
+
+# 6) Save configurations for easy reusability.
