298b606fa435c2ad2ff676e6388d74e9206a6850
Foalic <fionn.utsch@posteo.net>
2021-01-02 21:53:02 +0000

    - Added some comments for planning - Created a file explorer function - Created a button to browse files (incomplete - needs open file and action it to the window)

--- vwrite.py
+++ vwrite.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter import filedialog
 from reportlab.pdfgen import canvas
 from PIL import ImageTk, Image
 
@@ -9,31 +10,38 @@ WIN.title("VouchWrite")
 WIN.minsize(300, 300)
 WIN.configure(background="grey")
 
-startlabel = Label(WIN, text="Welcome to VouchWrite!")
-startlabel.pack()
+STARTLABEL = Label(WIN, text="Welcome to VouchWrite!")
+STARTLABEL.pack()
 
 # **Entry of template
 # !! Change sequence of commands
-canvas1 = Canvas(WIN)
-canvas1.pack()
+INITIAL_CANVAS = Canvas(WIN)
+INITIAL_CANVAS.pack()
 
 entry1 = Entry(WIN)
-canvas1.create_window(200, 140, window=entry1)
+INITIAL_CANVAS.create_window(200, 140, window=entry1)
 
-def send_img():
-    entry = entry1.get()
-    path = entry
+## Function to browse file explorer -- And open image in window to be edited
+def browseFiles():
+    filename = filedialog.askopenfilename(initialdir = "/", title = "Select a file", filetypes = (("Pdf", "*.pdf*"), ("Jpeg", "*.jpg*"), ("png", "*.png*"), ("all files", "*.*")))
+    #label_file_explorer.configure(text = "File opened: " + filename)                ## Change this line to adding path to button_open or opening file handle itself
+    filehandle = open(filename, "r")
+
+## !! Function that will print what is typed onto the opened file underneath, editing it
+def send_to_img():
+    path = entry1.get()             # Needs a file handle and open the file entered.
     return path
 
-button1 = Button(text='Add template', command=send_img)
-canvas1.create_window(200, 180, window=button1)
+button_explore = Button(WIN, text = "Browse Files", command = browseFiles)
+button_write = Button(WIN, text='Write', command=send_to_img)                   ## Need to use .drawString(x,y, text) to add string onto the pdf
+INITIAL_CANVAS.create_window(200, 180, window=button_explore)
 
 # 2) Upload a pdf (if image= would be great) and scale window to it
 # !!!**** Add this to definition in input box
-path = 'Background.png'
-img = ImageTk.PhotoImage(Image.open(path))
-imgwin = Label(WIN, image = img)
-imgwin.pack()
+# path = 'Background.png'
+# img = ImageTk.PhotoImage(Image.open(path))
+# imgwin = Label(WIN, image = img)
+# imgwin.pack()
 
 # 3) Allow input boxes to be created and pulled into place (-> use text-input cursor)
 
