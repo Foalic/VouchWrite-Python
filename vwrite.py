0a50465094466c00163dcc9a01b3933fea00f6a1
Foalic <fionn.utsch@posteo.net>
2021-01-03 15:18:17 +0000

    - Scrapped the original as it was messy and going in the wrong direction. - Added more readable and better functioning code - Incomplete - Editing and search need to be added as well as changing editing window into a modal window to block ROOT while running editing-window

--- vwrite.py
+++ vwrite.py
@@ -1,55 +1,49 @@
 from tkinter import *
 from tkinter import filedialog
-from reportlab.pdfgen import canvas
 from PIL import ImageTk, Image
 
-# 1) Create a window
+# Creating the root frame and window
+ROOT = Tk()
+ROOT.title("VouchWrite")
+ROOT.minsize(150, 150)
+ROOT.configure(background="white")
 
-WIN = Tk()
-WIN.title("VouchWrite")
-WIN.minsize(300, 300)
-WIN.configure(background="grey")
+# Creating an input box
+input_box = Entry(ROOT, width=30, border=2)
+input_box.pack()
+input_box.insert(0, "Enter file name")
 
-STARTLABEL = Label(WIN, text="Welcome to VouchWrite!")
-STARTLABEL.pack()
+# Function to open selected file in a new window
+def open_edit_win(filename):
+    global edit_img
+    FileWin = Toplevel()
+    FileWin.title("VouchWrite - Editing")
 
-# **Entry of template
-# !! Change sequence of commands
-INITIAL_CANVAS = Canvas(WIN)
-INITIAL_CANVAS.pack()
+    edit_img = ImageTk.PhotoImage(Image.open(filename))
+    img_label = Label(FileWin, image=editImg)
+    img_label.pack()
 
-entry1 = Entry(WIN)
-INITIAL_CANVAS.create_window(200, 140, window=entry1)
+# Function for browsing file explorer and opening image to be edited
+def select_file():               ## Block user from opening a second Editing window
+    input = inputBox.get()
+    if input == "Enter file name":
+        filename = filedialog.askopenfilename(initialdir = "/", title = "Select a file", filetypes = (("Jpeg", "*.jpg*"), ("png", "*.png*"), ("all files", "*.*")))
+        if filename != "":
+            startLabel.configure(text = "File opened: " + filename)
+            open_edit_win(filename)
 
-## Function to browse file explorer -- And open image in window to be edited
-def browseFiles():
-    filename = filedialog.askopenfilename(initialdir = "/", title = "Select a file", filetypes = (("Pdf", "*.pdf*"), ("Jpeg", "*.jpg*"), ("png", "*.png*"), ("all files", "*.*")))
-    #label_file_explorer.configure(text = "File opened: " + filename)                ## Change this line to adding path to button_open or opening file handle itself
-    filehandle = open(filename, "r")
-
-## !! Function that will print what is typed onto the opened file underneath, editing it
-def send_to_img():
-    path = entry1.get()             # Needs a file handle and open the file entered.
-    return path
-
-button_explore = Button(WIN, text = "Browse Files", command = browseFiles)
-button_write = Button(WIN, text='Write', command=send_to_img)                   ## Need to use .drawString(x,y, text) to add string onto the pdf
-INITIAL_CANVAS.create_window(200, 180, window=button_explore)
-
-# 2) Upload a pdf (if image= would be great) and scale window to it
-# !!!**** Add this to definition in input box
-# path = 'Background.png'
-# img = ImageTk.PhotoImage(Image.open(path))
-# imgwin = Label(WIN, image = img)
-# imgwin.pack()
-
-# 3) Allow input boxes to be created and pulled into place (-> use text-input cursor)
-
-# 4) Print input onto the pdf (canvas)
-
-# 5) [Merge pdf with written input or] create a new pdf
-
-# 6) Save configurations for easy reusability.
+        else:
+            pass
+    #else:
+        ## Need to be able to search for the file
 
 
-WIN.mainloop()
+# Creating label widget and packing it onto screen
+start_label = Label(ROOT, text="Welcome to Vouchwrite!")
+start_label.pack()
+
+# Creating button widget
+browse_button = Button(ROOT, text="Browse Files", padx=20, command=select_file)
+browse_button.pack()
+
+ROOT.mainloop()
