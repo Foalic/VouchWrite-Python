fa439b6a8b076a193982500a6571f7db48e932af
Foalic <fionn.utsch@posteo.net>
2021-01-04 19:29:36 +0000

    - Changed file to adhere to Python best practices, adding a main() function and conditional to run if main program
    - Removed unnecessary code
    
    - Added comments for some needed features

--- vwrite.py
+++ vwrite.py
@@ -5,52 +5,45 @@ from PIL import ImageTk, Image
 # Creating the root frame and window
 ROOT = Tk()
 ROOT.title("VouchWrite")
-ROOT.minsize(150, 150)
-ROOT.configure(background="white")
-
-
-# Creating an input box - Maybe just leave the browse button
-# input_box = Entry(ROOT, width=30, border=2)
-# input_box.pack()
-# input_box.insert(0, "Enter file name")
+ROOT.minsize(300, 150)
+ROOT.configure(background="black")
 
 # Function to open selected file in a new window
 def open_edit_win(filename):
     global edit_img
     FILEWIN = Toplevel()
     FILEWIN.title("VouchWrite - Editing")
-
-    # Scrollbars for FILEWIN
-    #scroll_vert = Scrollbar(img_label)
-    #scroll_vert.pack(side=RIGHT, fill=Y)
-    # # scroll_hori = Scrollbar(FILEWIN, orient=HORIZONTAL)
-    # # scroll_hori.pack()
+    FILEWIN.maxsize(1000, 500)
 
     edit_img = ImageTk.PhotoImage(Image.open(filename))
     img_label = Label(FILEWIN, image=edit_img)
     img_label.pack()
 
+    # # Scrollbar for FILEWIN
+    # scroll_vert = Scrollbar(FILEWIN)
+    # scroll_vert.pack(side=RIGHT, fill=Y)
+
 # Function for browsing file explorer and opening image to be edited
-def select_file():               ## Block user from opening a second Editing window
-    input = input_box.get()
-    if input == "Enter file name":
-        filename = filedialog.askopenfilename(initialdir = "/", title = "Select a file", filetypes = (("Jpeg", "*.jpg*"), ("png", "*.png*"), ("all files", "*.*")))
-        if filename != "":
-            start_label.configure(text = "File opened: " + filename)
-            open_edit_win(filename)
+def select_file():               ## Block user from opening a second Editing window - Create modal window
+    filename = filedialog.askopenfilename(initialdir = "/", title = "Select a file", filetypes = (("Jpeg", "*.jpg*"), ("png", "*.png*"), ("all files", "*.*")))
+    if filename != "":
+        start_label.configure(text = "File opened: " + filename)
+        open_edit_win(filename)
 
-        else:
-            pass
-    #else:
-        ## Need to be able to search for the file
+    else:
+        pass
+
+def main():
+    # Creating label widget and packing it onto screen
+    global start_label
+    start_label = Label(ROOT, text="Welcome to VouchWrite!", bg="black", fg="white", pady=30)
+    start_label.pack()
+
+    # Creating button widget
+    browse_button = Button(ROOT, text="Find File to Edit", padx=20, command=select_file, bg="white")
+    browse_button.pack()
+    ROOT.mainloop()
 
 
-# Creating label widget and packing it onto screen
-start_label = Label(ROOT, text="Welcome to Vouchwrite!")
-start_label.pack()
-
-# Creating button widget
-browse_button = Button(ROOT, text="Browse Files", padx=20, command=select_file)
-browse_button.pack()
-
-ROOT.mainloop()
+if __name__ == "__main__":
+    main()
